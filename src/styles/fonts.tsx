5f2acb4e9681923b04596f88a3505e67881e26fb
eniolajayi <ajayieniola16@gmail.com>
2023-09-11 12:08:29 +0100

    setup project

---
+++ src/styles/fonts.tsx
@@ -0,0 +1,11 @@
+import { DM_Sans, Poppins } from "next/font/google";
+
+const poppins400 = Poppins({ weight: "400" });
+const poppins500 = Poppins({ weight: "500" });
+const poppins600 = Poppins({ weight: "600" });
+
+const dmSans400 = DM_Sans({ weight: "400" });
+const dmSans500 = DM_Sans({ weight: "500" });
+const dmSans700 = DM_Sans({ weight: "700" });
+
+export { poppins400, poppins500, poppins600, dmSans400, dmSans500, dmSans700 };
