0e0ed07ace5d777ad93a06c2981fd21cb063fc37
eniolajayi <ajayieniola16@gmail.com>
2023-09-12 13:59:16 +0100

    update loading.ts file for search route

--- src/app/search/[query]/loading.tsx
+++ src/app/search/[query]/loading.tsx
@@ -1,4 +1,19 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function Loading() {
-    
-    return <>Loadding...</>
-  }
+  return (
+    <div className="px-4 py-3 md:container md:mx-auto min-h-[400px]">
+      <div className="grid grid-cols-4 gap-20 ">
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        <Skeleton className="h-[370px] w-[250px]" />
+        
+      </div>
+    </div>
+  );
+}
