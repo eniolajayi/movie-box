0eb7863699972a2258591940fb8d6a7108477f67
eniolajayi <ajayieniola16@gmail.com>
2023-09-12 11:31:23 +0100

    update search-bar component

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,10 +1,17 @@
 'use client';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 import { useRouter } from 'next/navigation';
 import { ChangeEvent, FormEvent, useState } from 'react';
+import { Input } from './ui/input';
+import { Label } from '@radix-ui/react-label';
+import { Button } from './ui/button';
+import { Search } from 'lucide-react';
 
-type SearchBarProps = {};
+type SearchBarProps = {
+  label: string;
+};
 
-export default function SearchBar({}: SearchBarProps) {
+export default function SearchBar({ label }: SearchBarProps) {
   const [query, setQuery] = useState('');
   const router = useRouter();
 
@@ -18,9 +25,21 @@ export default function SearchBar({}: SearchBarProps) {
   };
 
   return (
-    <form onSubmit={handleSearchSubmit}>
-      <input type="search" value={query} onChange={handleSearchInputChanges} />
-      <button type="submit">Search</button>
+    <form onSubmit={handleSearchSubmit} className='relative'>
+      <VisuallyHidden.Root>
+        <Label htmlFor="search">{label}</Label>
+      </VisuallyHidden.Root>
+      <Input
+        type="search"
+        id="search"
+        value={query}
+        placeholder={label}
+        onChange={handleSearchInputChanges}
+        className='transparent'
+      />
+      <Button size={'icon'} className='absolute top-0 right-1 bg-transparent '>
+        <Search className="w-4 h-4" />
+      </Button>
     </form>
   );
 }
