6d400ba56ad613aaf5bd69f372e73b283d342e62
eniolajayi <ajayieniola16@gmail.com>
2023-09-12 11:32:36 +0100

    use concise font objects

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,11 +1,17 @@
-import { DM_Sans, Poppins } from "next/font/google";
+import { DM_Sans, Poppins } from 'next/font/google';
 
-const poppins400 = Poppins({ weight: "400" });
-const poppins500 = Poppins({ weight: "500" });
-const poppins600 = Poppins({ weight: "600" });
+const poppins = Poppins({
+  weight: ['400', '500', '600'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
-const dmSans400 = DM_Sans({ weight: "400" });
-const dmSans500 = DM_Sans({ weight: "500" });
-const dmSans700 = DM_Sans({ weight: "700" });
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
-export { poppins400, poppins500, poppins600, dmSans400, dmSans500, dmSans700 };
+export { poppins, dmSans };
