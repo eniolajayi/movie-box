698ad9824334e4415e92dbd03f1c14da58e4411c
eniolajayi <ajayieniola16@gmail.com>
2023-09-13 20:50:31 +0100

    improve search page error message

--- src/app/search/[query]/error.tsx
+++ src/app/search/[query]/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+
 export default function Error({
   error,
   reset,
@@ -8,10 +10,12 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <p>{error.message}</p>
-      <button onClick={() => reset()}>Try again</button>
+    <div className="px-4 py-3 md:container md:mx-auto min-h-[300px] text-center flex gap-8 flex-col justify-center items-center">
+      <h2>Something went wrong! {error.message}</h2>
+      <p>
+        Ensure you're connected to the internet and refresh the page.
+      </p>
+      <Button variant={"secondary"} onClick={() => reset()}>Try Again</Button>
     </div>
   );
 }
